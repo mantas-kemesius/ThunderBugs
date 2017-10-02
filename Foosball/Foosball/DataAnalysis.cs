249802a7777e254e1c32658dd7579e7454ada0d3
Mantas Kemesius <mantas.kemesisu@gmail.com>
2017-10-02 08:45:19 +0300

    working master branch

---
+++ Foosball/Foosball/DataAnalysis.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foosball
+{
+    class DataAnalysis //prepare this class for also and database 
+    {
+        private string x;
+        private string y;
+
+        public void writeToCsv(string x, string y)
+        {
+            var csv = new StringBuilder();
+            //CSV
+            var first = x;
+            var second = y;
+            //Suggestion made by KyleMit
+            var newLine = string.Format("{0},{1}", first, second);
+            csv.AppendLine(newLine);
+
+            File.AppendAllText("C:/Users/Mantas/Desktop/Git/ThunderBugs/test.csv", csv.ToString());
+        }
+
+        public string getX()
+        {
+            return this.x;
+        }
+
+        public string getY()
+        {
+            return this.y;
+        }
+    }
+}
