a74f9bc624962bbeb0d65289715bb4e5797d9e18
DianaGro <diana.grochovskyte@gmail.com>
2017-10-06 15:22:44 +0300

    fix blueGoal

--- Foosball/Foosball/score.cs
+++ Foosball/Foosball/score.cs
@@ -21,10 +21,10 @@ namespace Foosball
 
         public void blueGoal(int x, int y)
         {
-            if (x >= 413)
-                if (x <= 420)
-                    if (y >= 85)
-                        if (y <= 135)
+            if (x <= 413)
+                if (x >= 420)
+                    if (y <= 85)
+                        if (y >= 135)
                             this._goalCount++;
         }
 
