47c3ef17c6cc32f8a22b9f2e49c97a4efb49a827
mantas-kemesius <mantas.kemesius@gmail.com>
2017-10-29 14:34:54 +0200

    Website ASP.NET with some design

---
+++ Foosball_app/Controllers/FoosballController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using System.Web.Services;
+
+namespace Foosball_app.Controllers
+{
+    public class FoosballController : Controller
+    {
+        // GET: Foosball
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+
+}
