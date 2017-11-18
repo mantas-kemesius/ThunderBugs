191ab61515ff9eb23c56ca59c16f06dcb58e1a66
DianaGro <diana.grochovskyte@gmail.com>
2017-11-18 13:49:46 +0200

    not done yet

---
+++ Foosball/Foosball.Test/ScoreTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Foosball.Test
+{
+    [TestClass]
+    public class ScoreTest
+    {
+        [TestMethod]
+        public void TestGoal()
+        {
+            int xCoord = 419;
+            int yCoord = 100;
+            int expextedGoal = 1;
+
+            int actualGoal = 
+        }
+    }
+}
