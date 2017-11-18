2cea79abba72e1f577cdcf3c8413b5c22ff8d9b7
DianaGro <diana.grochovskyte@gmail.com>
2017-11-18 13:16:45 +0200

    IsBetween Unit Test

---
+++ Foosball/Foosball.Test/IsBetweenTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Foosball.Test
+{
+    [TestClass]
+    public class IsBetweenTest
+    {
+        [TestMethod]
+        public void TestBetween()
+        {
+            bool expected = true;
+            int coord = 65;
+            int firstvalue = 50;
+            int secondvalue = 102;
+
+            bool actual = coord.Between(firstvalue, secondvalue);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
--- Foosball/Foosball/isBetween.cs
+++ Foosball/Foosball/isBetween.cs
@@ -9,7 +9,6 @@ namespace Foosball
 {
     public static class IsBetween
     {
-         
         public static bool Between(this int x, int value1, int value2)
         {
             return (x > value1 && x < value2) ? true : false;
